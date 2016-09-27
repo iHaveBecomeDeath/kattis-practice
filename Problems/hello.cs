c838e02b53cd97d2a0071bbbe4a0cd8e08dff421
Kalle Wallin <kalle.wallin@gmail.com>
2016-09-27 11:50:37 +0200

    Create hello.cs

---
+++ Problems/hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(new string(new[] { '\u0048', '\u0065', '\u006C', '\u006C', '\u006F', '\u0020', '\u0057', '\u006F', '\u0072', '\u006C', '\u0064', '\u0021' }));
+    }
+}
